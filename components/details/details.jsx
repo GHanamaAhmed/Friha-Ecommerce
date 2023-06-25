7ad1a7f78afc9abb5ab7aa7ece99021ce6ec661b
ghanama ahmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-06-25 23:23:55 +0100

    commit

--- components/details/details.jsx
+++ components/details/details.jsx
@@ -1,6 +1,4 @@
 "use client";
-import { Navigation, A11y, FreeMode } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import TitleSection from "../titleSection/titleSection";
@@ -12,6 +10,7 @@ import { useEffect, useReducer, useState } from "react";
 import { Tabs, TabsHeader, Tab } from "@material-tailwind/react";
 import Comments from "../comments/comments";
 import { selep } from "@@/lib/sleep";
+import Pictures from "./pictures";
 const initialState = (
   isShowComments,
   isLikeIt,
@@ -43,10 +42,10 @@ const reducer = (state, { type, payload }) => {
       return { ...state, isLoading };
   }
 };
-export default function Details({ idReel }) {
+export default function Details({ idReel, isOnlyPrudact }) {
   const [state, dispatch] = useReducer(
     reducer,
-    initialState(1, false, 121, 77, 78, true)
+    initialState(isOnlyPrudact ? 0 : 1, false, 121, 77, 78, true)
   );
   useEffect(() => {
     selep().then(() => {
@@ -65,70 +64,76 @@ export default function Details({ idReel }) {
   return (
     <div className="flex h-full w-full flex-col items-center">
       {state.isLoading ? (
-        <LoadingDetails />
+        <LoadingDetails isOnlyPrudact={isOnlyPrudact} />
       ) : (
         <>
-          <div className="w-11/12">
+          <div className={`w-11/12`}>
             <TitleSection title={"حذاء"} subtitle={"200DA"} />
           </div>
-          <div className="flex w-full flex-row items-center justify-center py-3">
-            <div className="flex w-11/12 flex-row-reverse justify-evenly rounded-md bg-card1 px-10">
-              <button
-                onClick={handleLike}
-                className="likeDesktop flex flex-row items-center justify-evenly gap-2"
-              >
-                <p
-                  className={`text-lg  font-bold ${
-                    state.isLikeIt ? "text-azure" : "text-lightSolid"
-                  } `}
+          {!isOnlyPrudact && (
+            <div className="flex w-full flex-row items-center justify-center py-3">
+              <div className="flex w-11/12 flex-row-reverse justify-evenly rounded-md bg-card1 px-10">
+                <button
+                  onClick={handleLike}
+                  className="likeDesktop flex flex-row items-center justify-evenly gap-2"
                 >
-                  {state.likes}
-                </p>
-                <AiFillLike
-                  size={40}
-                  className={`${state.isLikeIt ? "fill-azure" : "fill-white"}`}
-                />
-              </button>
-              <Tabs value={state.isShowComments} className="w-3/5">
-                <TabsHeader
-                  className="rounded-none border-blue-gray-50 bg-transparent p-0"
-                  indicatorProps={{
-                    className:
-                      "bg-transparent border-b-2 border-blue-500 shadow-none rounded-none",
-                  }}
-                >
-                  <Tab onClick={hideComments} value={0}>
-                    <div className="flex flex-row items-center justify-around gap-4">
-                      <p className={`text-lg  font-bold text-lightSolid `}>
-                        {state.left}
-                      </p>
-                      <FaShoppingCart size={40} color="white" />
-                    </div>
-                  </Tab>
-                  <Tab onClick={showComments} value={1}>
-                    <div className="flex flex-row items-center justify-around gap-4">
-                      <p className="text-lg  font-bold text-lightSolid">
-                        {state.comments}
-                      </p>
-                      <AiFillMessage size={40} color="white" />
-                    </div>
-                  </Tab>
-                </TabsHeader>
-              </Tabs>
-              <button className="shareDesktop">
-                <FaShare size={40} color="white" />
-              </button>
+                  <p
+                    className={`text-lg  font-bold ${
+                      state.isLikeIt ? "text-azure" : "text-lightSolid"
+                    } `}
+                  >
+                    {state.likes}
+                  </p>
+                  <AiFillLike
+                    size={40}
+                    className={`${
+                      state.isLikeIt ? "fill-azure" : "fill-white"
+                    }`}
+                  />
+                </button>
+                <Tabs value={state.isShowComments} className="w-3/5">
+                  <TabsHeader
+                    className="rounded-none border-blue-gray-50 bg-transparent p-0"
+                    indicatorProps={{
+                      className:
+                        "bg-transparent border-b-2 border-blue-500 shadow-none rounded-none",
+                    }}
+                  >
+                    <Tab onClick={hideComments} value={0}>
+                      <div className="flex flex-row items-center justify-around gap-4">
+                        <p className={`text-lg  font-bold text-lightSolid `}>
+                          {state.left}
+                        </p>
+                        <FaShoppingCart size={40} color="white" />
+                      </div>
+                    </Tab>
+                    <Tab onClick={showComments} value={1}>
+                      <div className="flex flex-row items-center justify-around gap-4">
+                        <p className="text-lg  font-bold text-lightSolid">
+                          {state.comments}
+                        </p>
+                        <AiFillMessage size={40} color="white" />
+                      </div>
+                    </Tab>
+                  </TabsHeader>
+                </Tabs>
+                <button className="shareDesktop">
+                  <FaShare size={40} color="white" />
+                </button>
+              </div>
             </div>
-          </div>
+          )}
         </>
       )}
       {!state.isShowComments && (
-        <div className="grid h-full w-full grid-cols-12 grid-rows-1 pt-10">
+        <div
+          className={`h-full w-full`}
+        >
           <div className="col-span-8 flex h-full w-full flex-col  items-center">
             <div className="flex h-5/6 w-full flex-col items-center justify-start">
               <div className="flex w-full flex-col items-start gap-3">
                 <div className="w-full px-5">
-                  <div className="relative h-64 w-10/12">
+                  <div className="relative h-64 w-8/12">
                     <Image
                       fill={true}
                       priority
@@ -137,6 +142,9 @@ export default function Details({ idReel }) {
                       className="h-full w-full rounded-md"
                     />
                   </div>
+                </div>
+                <div className="w-11/12 px-5">
+                  <Pictures direction={"horizontal"} />
                 </div>
                 <div className="flex w-fit gap-3 px-5">
                   <Example />
@@ -154,28 +162,6 @@ export default function Details({ idReel }) {
               </div>
             </div>
           </div>
-          <div className="col-span-4 flex h-full w-full justify-center">
-            <div className="h-96">
-              <Swiper
-                className="h-full"
-                modules={[Navigation, A11y, FreeMode]}
-                spaceBetween={50}
-                slidesPerView={4}
-                direction="vertical"
-              >
-                <SwiperSlide>
-                  <Image
-                    width={100}
-                    height={100}
-                    priority
-                    src={"/res/item.png"}
-                    alt=""
-                    className="rounded-lg"
-                  />
-                </SwiperSlide>
-              </Swiper>
-            </div>
-          </div>
         </div>
       )}
       {state.isShowComments && (
@@ -189,16 +175,18 @@ export default function Details({ idReel }) {
   );
 }
 
-function LoadingDetails() {
+function LoadingDetails({ isOnlyPrudact }) {
   return (
-    <div className="flex h-full w-full animate-pulse flex-col pt-7 items-center">
+    <div className="flex h-full w-full animate-pulse flex-col items-center pt-7">
       <div className="w-11/12 pt-4">
         <div class="mb-4 h-2.5 w-48 rounded-full bg-gray-700"></div>
         <div class="mb-4 h-2 w-40 rounded-full bg-gray-700"></div>
       </div>
-      <div className="flex w-full flex-row items-center justify-center py-3">
-        <div className="mb-4 flex h-14 w-11/12 flex-row-reverse justify-evenly rounded-md bg-gray-700 px-10"></div>
-      </div>
+      {!isOnlyPrudact && (
+        <div className="flex w-full flex-row items-center justify-center py-3">
+          <div className="mb-4 flex h-14 w-11/12 flex-row-reverse justify-evenly rounded-md bg-gray-700 px-10"></div>
+        </div>
+      )}
     </div>
   );
 }
--- src/app/(home)/products/[product]/components/desktop/desktopprudact.jsx
+++ src/app/(home)/products/[product]/components/desktop/desktopprudact.jsx
@@ -1,10 +1,14 @@
+import Comments from "@@/components/comments/comments";
 import Details from "@@/components/details/details";
 
 export default function Desktopprudact() {
   return (
-    <div className="pt-20">
-      <div className="w-10/12">
-        <Details />
+    <div className="grid grid-cols-12 pt-14">
+      <div className="col-span-7">
+        <Details isOnlyPrudact={true}/>
+      </div>
+      <div className="col-span-5">
+            <Comments/>
       </div>
     </div>
   )
