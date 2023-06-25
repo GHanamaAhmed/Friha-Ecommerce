1314ced65952278a4b059ab98bf658fd7568eaab
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-06-25 21:38:44 +0100

    6/25/2023

--- src/app/component/grid/CardGrid.jsx
+++ src/app/component/grid/CardGrid.jsx
@@ -13,33 +13,29 @@ export default function CardGrid() {
   }, []);
   const content = () => {
     return !isLoading ? (
-      <>
-        <Search />
-        <div className="grid grid-cols-2 place-items-center gap-6 px-3 sm:grid-cols-2 md:grid-cols-3 md:gap-8 lg:grid-cols-4">
-          {[...Array(10)].map((e, i) => {
-            return (
-              <Card
-                key={i}
-                title={"producto"}
-                subtitle={"free!!!!"}
-                img={img1}
-              />
-            );
-          })}
-        </div>
-        <div className="flex w-full justify-center py-3">
-          <Button
-            variant="outlined"
-            size="md"
-            className={`font-Hacen-Tunisia border-lightContent  text-lightContent `}
-          >
-            المزيد
-          </Button>
-        </div>
-      </>
-    ) : (
-      <CardGridLoading />
-    );
+			<>
+				<Search />
+				<div className="grid grid-cols-2 place-items-center gap-6 px-3 sm:grid-cols-2 md:grid-cols-3 md:gap-8 lg:grid-cols-4">
+					{[...Array(10)].map((e, i) => {
+						return <Card key={i} />;
+					})}
+				</div>
+				<div className="flex w-full justify-center py-3">
+					<Button
+						variant="outlined"
+						size="md"
+						className={`font-Hacen-Tunisia border-lightContent ${
+
+								"text-lightContent Focus:border-none focus:bg-scandaryColor focus:text-white"
+						} focus:ring-0`}
+					>
+						المزيد
+					</Button>
+				</div>
+			</>
+		) : (
+			<CardGridLoading />
+		);
   };
   return content();
 }
