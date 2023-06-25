a71ae61c8a6832cc8552634d915688d36d7d806f
ghanama ahmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-06-25 17:35:21 +0100

    commti

--- src/app/(home)/products/[product]/components/mobile/Pictures.jsx
+++ src/app/(home)/products/[product]/components/mobile/Pictures.jsx
@@ -9,21 +9,29 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/free-mode";
 import img1 from "@@/public/res/item.png";
-import { useWidth } from "@@/lib/hooks/useWidth";
-export default function Pictures({ onClick, pictures }) {
-  const { width } = useWidth();
+export default function Pictures({ onClick, pictures, idPrudacts }) {
   const click = (e) => {
     e.preventDefault();
     onClick(e.currentTarget.src);
   };
   return (
-    <div className={`my-3 w-full overflow-clip px-4`}>
+    <div className={`my-3 w-full px-4`}>
       <Swiper
-        slidesPerView={width < 360 ? 2 : width < 500 ? 3 : 5}
+        slidesPerView={3}
         scrollbar={{ draggable: true }}
         className="MySwiper"
         freeMode={true}
         modules={[FreeMode, Navigation]}
+        breakpoints={{
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+          },
+        }}
       >
         <SwiperSlide>
           <Image
--- src/app/(home)/products/[product]/components/mobile/mobilePrudact.jsx
+++ src/app/(home)/products/[product]/components/mobile/mobilePrudact.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import Pictures from "./swiper";
-import Menus from "@@/components/details/menu"
+import Pictures from "./Pictures";
+import Menus from "@@/components/details/menu";
 import Par from "./par";
 import MobileComents from "@@/components/comments/mobileComents";
 import TitleSection from "../../../../../../../components/titleSection/titleSection";
@@ -21,38 +21,38 @@ export default function MobilePrudact() {
   };
   return (
     <div className="relative overflow-hidden">
-      <div className="flex h-screen overflow-y-auto  flex-col items-center justify-center pt-20">
-      <div className="relative flex w-full flex-col gap-3">
-        <div className="relative h-80 w-full">
-          <Image fill={true} src={currentPicture} alt="" />
-        </div>
-        <div className="w-full">
-          <div className="flex w-full items-center justify-between px-2 text-end">
-            <TitleSection title={"adidas"} subtitle={"20 da"} />
+      <div className="flex h-screen flex-col  items-center justify-center overflow-y-auto pt-44">
+        <div className="relative flex w-full flex-col gap-3">
+          <div className="relative h-80 w-full">
+            <Image fill={true} src={currentPicture} alt="" />
+          </div>
+          <div className="w-full">
+            <div className="flex w-full items-center justify-between px-2 text-end">
+              <TitleSection title={"adidas"} subtitle={"20 da"} />
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleComments(true);
+                }}
+              >
+                <AiFillMessage size={40} color="white" />
+              </button>
+            </div>
             <button
-              onClick={(e) => {
-                e.preventDefault();
-                handleComments(true);
-              }}
+              onClick={() => router.back()}
+              className="absolute left-4 top-4"
             >
-              <AiFillMessage size={40} color="white" />
+              <BsArrowLeft color="white" size="25px" />
             </button>
           </div>
-          <button
-            onClick={() => router.back()}
-            className="absolute left-4 top-4"
-          >
-            <BsArrowLeft color="white" size="25px" />
-          </button>
         </div>
+        <Pictures onClick={changeCurrentPicture} />
+        <div className="flex w-full justify-between px-4">
+          <Menus />
+        </div>
+        <Par />
+        <MobileComents show={isShowComments} onClose={handleComments} />
       </div>
-      <Pictures onClick={changeCurrentPicture} />
-      <div className="flex w-full justify-between px-4">
-        <Menus />
-      </div>
-      <Par />
-      <MobileComents show={isShowComments} onClose={handleComments}/>
-    </div>
     </div>
   );
 }
