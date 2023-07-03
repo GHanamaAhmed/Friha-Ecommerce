699d6edefe75a4b16f043fdf310184173b8ddc34
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-07-03 20:44:39 +0100

    7/3/2023

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
 	deploy: {
 		production: {
-			key: "ssh-key-2023-07-01 (2).key",
+			key: "ssh-key-2023-07-01.key",
 			user: "ubuntu",
 			host: "158.180.30.34",
 			ref: "origin/main",
@@ -17,7 +17,7 @@ module.exports = {
 			"post-deploy":
 				"source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
 			"pre-setup": "",
-      "ssh_options": "forwardAgent=yes"
+			ssh_options: "forwardAgent=yes",
 		},
 	},
 };
