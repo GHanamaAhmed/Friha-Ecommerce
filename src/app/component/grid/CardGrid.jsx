64eb9a7902cd9032eb48f4970c92cf9a77fde9e5
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-07-03 21:20:33 +0100

    7/3/2023 3

--- src/app/component/grid/CardGrid.jsx
+++ src/app/component/grid/CardGrid.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import img1 from "../swipers/img/reels.png";
 import Card from "./card";
 import Search from "./search";
 import { selep } from "../../../../lib/sleep";
