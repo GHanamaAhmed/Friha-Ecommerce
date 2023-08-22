fee30efb006d72af450ee32a95ef562d6f228210
ghanama ahmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-08-22 17:37:45 +0100

    update

--- src/app/component/grid/search.jsx
+++ src/app/component/grid/search.jsx
@@ -48,7 +48,7 @@ export default function Search({
                 {type}{" "}
               </Button>
             </MenuHandler>
-            <MenuList className="font-Hacen-Tunisia bg-card1 text-lightSolid">
+            <MenuList className="font-Hacen-Tunisia bg-card1 text-lightSolid max-h-72">
               {["الكل", ...types]?.map((e, i) => (
                 <MenuItem
                   onClick={() => {
