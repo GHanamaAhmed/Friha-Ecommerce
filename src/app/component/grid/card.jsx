8792b90bd7db07bd03ad9e5fa5bd429161cf2250
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-11-01 13:02:08 +0000

    delete quantity

--- src/app/component/grid/card.jsx
+++ src/app/component/grid/card.jsx
@@ -104,7 +104,6 @@ export default memo(function Card({
           )}
         </div>
         <div className="flex w-full items-center justify-between border-t-[1px] border-card2 py-px md:hidden md:py-2">
-          <p className="text-sm text-white md:text-lg">الكمية: {quntity}</p>
           <Tooltip
             content={`${!isLike ? "اعجاب" : "تم الاعجاب"}`}
             placement="top"
